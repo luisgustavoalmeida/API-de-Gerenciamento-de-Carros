7ad6edc3d997c396e215b0f3cd5acaa2da060061
Luís Gustavo de Almeida <luis.gustavo@engenharia.ufjf.br>
2023-09-27 20:30:55 -0300

    melhorias

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Importar as bibliotecas necessárias
 from flask import Flask, make_response, jsonify, request
-from bd import Carros  # Simula um banco de dados
+from data import Carros  # Simula um banco de dados
 
 
 # Criar uma instância do aplicativo Flask
@@ -21,6 +21,41 @@ def get_carros():
                 dados=Carros
             )
         )
+    except Exception as e:
+        # Tratar outros erros desconhecidos retornando uma resposta 500 Internal Server Error
+        return make_response(
+            jsonify(
+                mensagem='Erro interno do servidor: ' + str(e),
+            ),
+            500
+        )
+# Definir uma rota para lidar com solicitações GET para /carros/<int:id>
+@app.route('/carros/<int:id>', methods=['GET'])
+def get_carro_by_id(id):
+    try:
+        # Pesquisar o carro com a ID fornecida na lista de carros
+        carro_encontrado = None
+        for carro in Carros:
+            if carro['id'] == id:
+                carro_encontrado = carro
+                break
+
+        if carro_encontrado:
+            # Se o carro for encontrado, retornar as informações do carro
+            return make_response(
+                jsonify(
+                    mensagem='Carro encontrado.',
+                    dados=carro_encontrado
+                )
+            )
+        else:
+            # Se o carro não for encontrado, retornar uma resposta 404 Not Found
+            return make_response(
+                jsonify(
+                    mensagem='Carro não encontrado.'
+                ),
+                404
+            )
     except Exception as e:
         # Tratar outros erros desconhecidos retornando uma resposta 500 Internal Server Error
         return make_response(
@@ -65,6 +100,44 @@ def create_carro():
             500
         )
 
+# Definir uma rota para lidar com solicitações DELETE para /carros/<int:id>
+@app.route('/carros/<int:id>', methods=['DELETE'])
+def delete_carro(id):
+    try:
+        # Procurar o carro com a ID fornecida na lista de carros
+        carro_encontrado = None
+        for carro in Carros:
+            if carro['id'] == id:
+                carro_encontrado = carro
+                break
+
+        if carro_encontrado:
+            # Se o carro for encontrado, removê-lo da lista de carros
+            Carros.remove(carro_encontrado)
+
+            # Criar uma resposta JSON que confirma a exclusão do carro
+            return make_response(
+                jsonify(
+                    mensagem='Carro excluído com sucesso.',
+                    dados=carro_encontrado
+                )
+            )
+        else:
+            # Se o carro não for encontrado, retornar uma resposta 404 Not Found
+            return make_response(
+                jsonify(
+                    mensagem='Carro não encontrado.'
+                ),
+                404
+            )
+    except Exception as e:
+        # Tratar outros erros desconhecidos retornando uma resposta 500 Internal Server Error
+        return make_response(
+            jsonify(
+                mensagem='Erro interno do servidor: ' + str(e),
+            ),
+            500
+        )
 
 # Iniciar o servidor Flask
 app.run()
